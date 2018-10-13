a16d7f6df943908d37c7503392806f64110b25b1
Lior Regev <lioregev@gmail.com>
2018-10-13 20:09:40 +0300

    Initial client

---
+++ src/main/scala/com/liorregev/rssdownloader/transmission/Client.scala
@@ -0,0 +1,45 @@
+package com.liorregev.rssdownloader.transmission
+
+import com.liorregev.rssdownloader.transmission.domain.{Request, RequestType, Response}
+import play.api.libs.json._
+import play.api.libs.ws.{JsonBodyReadables, JsonBodyWritables, StandaloneWSClient}
+
+import scala.concurrent.{ExecutionContext, Future}
+import cats.syntax.either._
+
+sealed trait ClientError
+final case class ResponseParseError(jsError: JsError) extends ClientError
+
+class Client(url: String)(implicit wsClient: StandaloneWSClient) extends JsonBodyWritables with JsonBodyReadables {
+  var csrfToken: Option[String] = None
+
+  def request[T <: RequestType, Resp <: Response[T]](req: Request[T, Resp])
+                                                    (implicit writes: Writes[Request[T, Resp]],
+                                                     ec: ExecutionContext): Future[Either[ClientError, Resp]] = {
+    runRequest(req)
+      .map(req.responseReads.reads)
+      .map {
+        case JsSuccess(value, _) => value.asRight[ClientError]
+        case err: JsError => ResponseParseError(err).asLeft[Resp]
+      }
+  }
+
+  private def runRequest[Resp <: Response[T], T <: RequestType](req: Request[T, Resp])
+                                                               (implicit writes: Writes[Request[T, Resp]], ec: ExecutionContext): Future[JsValue] = {
+    wsClient.url(url)
+      .addHttpHeaders(csrfToken.map(Client.CSRF_HEADER -> _).toSeq: _*)
+      .post(Json.toJson(req))
+      .flatMap(resp => {
+        if (resp.status == 409) {
+          csrfToken = resp.header(Client.CSRF_HEADER)
+          runRequest(req)
+        } else {
+          Future(resp.body[JsValue])
+        }
+      })
+  }
+}
+
+object Client {
+  val CSRF_HEADER = "X-Transmission-Session-Id"
+}
